b08131689c13f3a40a9468af3cfcda7da6c82bc2
ericjjung <ericjung@github.com>
2022-08-19 18:05:11 +0900

    브렌치 용 커밋

--- src/main/java/com/example/gentle/service/MemberService.java
+++ src/main/java/com/example/gentle/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.gentle.service;
 
 
+import com.example.gentle.jwt.TokenProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;

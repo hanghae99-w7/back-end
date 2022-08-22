7d650cad74f1d5b87e58cf500b36226e4829d5b2
ericjjung <ericjung@github.com>
2022-08-22 16:38:35 +0900

    제품 상세 페이지 조회

---
+++ src/main/java/com/example/gentle/service/DetailItemPageService.java
@@ -0,0 +1,25 @@
+package com.example.gentle.service;
+
+import com.example.gentle.domain.ItemInfo;
+import com.example.gentle.dto.responseDto.Message;
+import com.example.gentle.repository.ItemInfoRepository;
+import com.example.gentle.repository.MemberRepository;
+import com.sparta.miniproject_movie_study_01.controller.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DetailItemPageService {
+
+   private final ItemInfoRepository itemInfoRepository;
+
+   public ResponseEntity<?> getDatailItemPage(Long itemId){
+       ItemInfo itemInfo1 =itemInfoRepository.findById(itemId).orElseThrow(
+               ()-> new IllegalArgumentException("없는 아이템 정보입니다.")
+       );
+       return new ResponseEntity<>(Message.success(itemInfo1), HttpStatus.OK);
+   }
+}
